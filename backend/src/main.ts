3d59131489178e7091731a822948990e12fbc119
Emmanuel Van Dyk <emmaaavd@gmail.com>
2025-04-15 15:38:33 -0300

    Refactor main.ts to improve application initialization and configuration. Added global validation pipes, CORS support, and logging with Morgan. Ensured app instance is reused for serverless environments.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,19 +3,25 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import morgan from 'morgan';
+import { CORS } from './constants';
+import { INestApplication } from '@nestjs/common';
 
 // Para entornos serverless, es útil guardar y reutilizar la instancia
-let app;
+let app: INestApplication;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  if (!app) {
+    app = await NestFactory.create(AppModule);
 
-  app.use(morgan('dev'));
+    app.use(morgan('dev'));
+    app.useGlobalPipes(new ValidationPipe());
+    app.enableCors(CORS);
 
-  app.enableCors(CORS);
-
-  app.setGlobalPrefix('api');
+    app.setGlobalPrefix('api');
+  }
 
   await app.listen(process.env.PORT ?? 8080);
+  return app;
 }
 bootstrap();
